f55cbf22e313ebf87b740089d196c75bf4ce4ec9
VishalBala@Office-Jeevijay <vishal.bala.100@gmail.com>
2025-05-27 18:29:24 +0530

    dep fix 12

--- src/Components/Chat/NewChat/messageHelper.js
+++ src/Components/Chat/NewChat/messageHelper.js
@@ -1,61 +1,61 @@
-export const updateMessageSentByLoggedInUser = (MESSAGE, CURRENT_USER) => {
-  const messageUl = document.getElementById("message-ul");
-  if (messageUl) {
-    const li = document.createElement("li");
-    li.className = "hvt d-flex flex-column message align-self-end";
-    li.style.maxWidth = "65%";
+// export const updateMessageSentByLoggedInUser = (MESSAGE, CURRENT_USER) => {
+//   const messageUl = document.getElementById("message-ul");
+//   if (messageUl) {
+//     const li = document.createElement("li");
+//     li.className = "hvt d-flex flex-column message align-self-end";
+//     li.style.maxWidth = "65%";
 
-    li.innerHTML = `
-      <div class=" d-flex flex-row flex-row-reverse align-items-end gap-2">
-        <div>
-          <img class="rounded-circle" height="25" width="25" src="${
-            CURRENT_USER.image_url
-          }" alt="image" />
-        </div>
-        <div class="d-flex flex-column">
-          <div style="font-size: 12px;" class="align-self-end">
-            You
-          </div>
-          <div class="message bg-white rounded-3 py-1 px-3">
-            <div>${MESSAGE.message}</div>
-            <div style="font-size: 10px;">${new Date().toLocaleString()}</div>
-          </div>
-        </div>
-      </div>
-    `;
+//     li.innerHTML = `
+//       <div class=" d-flex flex-row flex-row-reverse align-items-end gap-2">
+//         <div>
+//           <img class="rounded-circle" height="25" width="25" src="${
+//             CURRENT_USER.image_url
+//           }" alt="image" />
+//         </div>
+//         <div class="d-flex flex-column">
+//           <div style="font-size: 12px;" class="align-self-end">
+//             You
+//           </div>
+//           <div class="message bg-white rounded-3 py-1 px-3">
+//             <div>${MESSAGE.message}</div>
+//             <div style="font-size: 10px;">${new Date().toLocaleString()}</div>
+//           </div>
+//         </div>
+//       </div>
+//     `;
 
-    messageUl.appendChild(li);
-    messageUl.scrollTop = messageUl.scrollHeight; // Auto-scroll to bottom
-  }
-};
+//     messageUl.appendChild(li);
+//     messageUl.scrollTop = messageUl.scrollHeight; // Auto-scroll to bottom
+//   }
+// };
 
-export const updateMessageSentBySelectedUser = (MESSAGE, SELECTED_USER) => {
-  const messageUl = document.getElementById("message-ul");
-  if (messageUl) {
-    const li = document.createElement("li");
-    li.className = "hvt d-flex flex-column message align-self-end";
-    li.style.maxWidth = "65%";
+// export const updateMessageSentBySelectedUser = (MESSAGE, SELECTED_USER) => {
+//   const messageUl = document.getElementById("message-ul");
+//   if (messageUl) {
+//     const li = document.createElement("li");
+//     li.className = "hvt d-flex flex-column message align-self-end";
+//     li.style.maxWidth = "65%";
 
-    li.innerHTML = `
-      <div class="d-flex flex-row flex-row align-items-start gap-2">
-        <div>
-          <img class="rounded-circle" height="25" width="25" src="${
-            SELECTED_USER.image_url
-          }" alt="image" />
-        </div>
-        <div class="d-flex flex-column">
-          <div style="font-size: 12px;" class="align-self-start">
-            You
-          </div>
-          <div class="message bg-white rounded-3 py-1 px-3">
-            <div>${MESSAGE.message}</div>
-            <div style="font-size: 10px;">${new Date().toLocaleString()}</div>
-          </div>
-        </div>
-      </div>
-    `;
+//     li.innerHTML = `
+//       <div class="d-flex flex-row flex-row align-items-start gap-2">
+//         <div>
+//           <img class="rounded-circle" height="25" width="25" src="${
+//             SELECTED_USER.image_url
+//           }" alt="image" />
+//         </div>
+//         <div class="d-flex flex-column">
+//           <div style="font-size: 12px;" class="align-self-start">
+//             You
+//           </div>
+//           <div class="message bg-white rounded-3 py-1 px-3">
+//             <div>${MESSAGE.message}</div>
+//             <div style="font-size: 10px;">${new Date().toLocaleString()}</div>
+//           </div>
+//         </div>
+//       </div>
+//     `;
 
-    messageUl.appendChild(li);
-    messageUl.scrollTop = messageUl.scrollHeight; // Auto-scroll to bottom
-  }
-};
+//     messageUl.appendChild(li);
+//     messageUl.scrollTop = messageUl.scrollHeight; // Auto-scroll to bottom
+//   }
+// };
