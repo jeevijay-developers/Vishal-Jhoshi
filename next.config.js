1f45e6b75ce915be06ae289872d7827a20e4e45f
VishalBala@Office-Jeevijay <vishal.bala.100@gmail.com>
2025-05-28 11:57:51 +0530

    dep fix 18

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  reactStrictMode: false,
-  output: "standalone",
+  // ✅ No need for `output: "export"` or `"standalone"` unless you're using Docker
 };
 
 module.exports = nextConfig;
