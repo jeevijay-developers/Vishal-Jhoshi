005849d4c62eecb0166dec0f79573310218ced45
Abhishek Yadav <monuy8830@gmail.com>
2025-05-28 13:14:35 +0530

    refactor: comment out Dashboard components in test dashboard page

--- src/app/(MainBody)/(general)/test-dashboard/page.tsx
+++ src/app/(MainBody)/(general)/test-dashboard/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import DasbBoardHeader from "../../../../Layout/dashboard/DasbBoardHeader";
-import DashboardWrapper from "../../../../Layout/dashboard/DashboardWrapper";
+// import DasbBoardHeader from "../../../../Layout/dashboard/DasbBoardHeader";
+// import DashboardWrapper from "../../../../Layout/dashboard/DashboardWrapper";
 import React from "react";
 
 const page = () => {
   return (
     <div className="p-0 m-0">
-      <DasbBoardHeader />
-      <DashboardWrapper />
+      {/* <DasbBoardHeader />
+      <DashboardWrapper /> */}
     </div>
   );
 };
