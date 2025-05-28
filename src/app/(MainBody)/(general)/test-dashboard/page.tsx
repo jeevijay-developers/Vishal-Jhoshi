f0e8f662bfe9fdba6541e23bdc0b0d04c1d3fefb
Abhishek Yadav <monuy8830@gmail.com>
2025-05-28 15:11:10 +0530

    refactor: update imports in DashboardWrapper and test dashboard page for dynamic loading

--- src/app/(MainBody)/(general)/test-dashboard/page.tsx
+++ src/app/(MainBody)/(general)/test-dashboard/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 // import DasbBoardHeader from "@/Layout/dashboard/DasbBoardHeader";
-import DashboardWrapper from "@/Layout/dashboard/DashboardWrapper";
+import dynamic from "next/dynamic";
+const DashboardWrapper = dynamic(
+  () => import("@/Layout/dashboard/DashboardWrapper"),
+  { ssr: false }
+);
 // import DasbBoardHeader from "../../../../Layout/dashboard/DasbBoardHeader";
 // import DashboardWrapper from "../../../../Layout/dashboard/DashboardWrapper";
 import React from "react";
