38867ccb15556436e9092489b10cd582bfef7826
Abhishek Yadav <monuy8830@gmail.com>
2025-05-28 13:04:48 +0530

    refactor: comment out Bootstrap bundle dynamic import and effect

--- src/Layout/dashboard/DashboardWrapper.tsx
+++ src/Layout/dashboard/DashboardWrapper.tsx
@@ -4,10 +4,10 @@ import DsbSidebar from "./DsbSidebar";
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import dynamic from "next/dynamic";
-const BootstrapBundle = dynamic(
-  () => import("bootstrap/dist/js/bootstrap.bundle.min.js"),
-  { ssr: false }
-);
+// const BootstrapBundle = dynamic(
+//   () => import("bootstrap/dist/js/bootstrap.bundle.min.js"),
+//   { ssr: false }
+// );
 import StudentMain from "./StudentMain";
 import { useSelector } from "react-redux";
 import AdminSidebar from "./admin/AdminSidebar";
@@ -17,9 +17,9 @@ const DashboardWrapper = () => {
   const [show, setShow] = React.useState("PROFILE");
   const user = useSelector((state: any) => state.user);
   console.log(user);
-  React.useEffect(() => {
-    BootstrapBundle;
-  }, []);
+  // React.useEffect(() => {
+  //   BootstrapBundle;
+  // }, []);
 
   return (
     <div className="container-fluid">
